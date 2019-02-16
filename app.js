7f54ce7a558a7f4c18fc79a21fa07c6dea0a575d
Raghunath Vadlakonda <raghu@Raghunaths-MacBook-Air.local>
2019-02-16 13:12:39 +0530

    forEach and sum of array

--- app.js
+++ app.js
@@ -4,4 +4,15 @@
 var names = ['peter', 'sam', 'harry', 'nan', 'emma'];
 names.forEach((name)=> {
     console.log(name);
-})
+});
+
+//sum of array
+
+var numbers = [1,2,3,4,5];
+var sum=0;
+numbers.forEach((number) => {
+    console.log(number);
+    sum+=number;
+});
+//console.log(sum);
+console.log(`result: ${sum} `);
